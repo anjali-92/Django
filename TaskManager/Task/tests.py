3d1757be39282571dbf241fa3d585a591af99a26
Anjali Pardeshi <anjalipardeshi92@gmail.com>
2016-04-11 16:35:19 +0530

    test case

--- TaskManager/Task/tests.py
+++ TaskManager/Task/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory, APIClient, APITestCase
+from rest_framework import status
+from rest_framework_jwt.compat import get_user_model
+User = get_user_model()
+
 '''
+Database is dropped after every test case is executed
+
 login_url='/api-token-auth/'
 data={'username':'admin','password':'admin'}
 response = client.post(login_url,dataw)
@@ -19,16 +25,22 @@ models
 class AuthTokenTests(APITestCase):
     def setUp(self):
         print "in setup"
+        self.email = 'admin@admin.com'
+        self.username = 'admin'
+        self.password = 'admin'
+        self.user = User.objects.create_user(self.username, self.email, self.password)
         self.factory = APIRequestFactory()
         self.api_client = APIClient()
-        login_url='/api-token-auth/'
-        data={'username':'admin','password':'admin'}
+        self.login_url='/api-token-auth/'
+        self.data={'username':'admin','password':'admin'}
 
     def test_get_token(self):
         """
             to get auth token of JWT from middleware
         """
         print "in get token"
+        response = self.api_client.post(self.login_url,self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         pass
 
     def tearDown(self):
